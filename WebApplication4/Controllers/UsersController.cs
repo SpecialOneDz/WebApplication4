6326349188f7222a2b9f9eedbb3506e3db5e3efc
NT00 <NT00@SPECIALONELAB>
2017-05-08 20:37:30 +0100

    user + init 7

--- WebApplication4/Controllers/UsersController.cs
+++ WebApplication4/Controllers/UsersController.cs
@@ -9,8 +9,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using WebApplication4.Models;
 
-namespace WebApplication4.Models
+namespace WebApplication4.Controllers
 {
     public class UsersController : ApiController
     {
